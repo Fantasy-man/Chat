43acc9587f3fa9c214104ba44ba2598f9e67bdfd
Marcel <Fantasy-man@t-online.de>
2014-03-10 22:05:57 +0100

    Noch mit Fehlern

--- Chat/gen/de/marcel/chat/BuildConfig.java
+++ Chat/gen/de/marcel/chat/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.chat;
+package de.marcel.chat;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- Chat/src/de/marcel/chat/ChatActivity.java
+++ Chat/src/de/marcel/chat/ChatActivity.java
@@ -15,7 +15,7 @@ public class ChatActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.chat, menu);
+		getMenuInflater().inflate(R.menu., menu);
 		return true;
 	}
 

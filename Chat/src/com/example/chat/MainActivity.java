70faa71bf7749e1654fa8a0979ffcd68e809d556
Marcel <Fantasy-man@t-online.de>
2014-03-10 20:08:09 +0100

    ÄNderung abc

--- Chat/src/com/example/chat/MainActivity.java
+++ Chat/src/com/example/chat/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+	} // sdflkasdj
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
